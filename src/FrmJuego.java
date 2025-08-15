1a6c354f50d2df4daf7ff58d94602869c41facba
fray osorio <frayosorio@gmail.com>
2025-08-15 13:17:31 -0500

    Se termina de construir la interfaz grafica

--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,6 +1,14 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 public class FrmJuego extends JFrame {
@@ -19,6 +27,81 @@ public class FrmJuego extends JFrame {
                 imgDado.getIconWidth(),
                 imgDado.getIconHeight());
         getContentPane().add(lblDado1);
+
+        JLabel lblDado2 = new JLabel();
+        lblDado2.setIcon(imgDado);
+        lblDado2.setBounds(20 + imgDado.getIconWidth(), 10,
+                imgDado.getIconWidth(),
+                imgDado.getIconHeight());
+        getContentPane().add(lblDado2);
+
+        JLabel lblTituloLanzamientos = new JLabel("Lanzamientos");
+        lblTituloLanzamientos.setBounds(30 + 2 * imgDado.getIconWidth(), 10,
+                100, 25);
+        getContentPane().add(lblTituloLanzamientos);
+
+        JLabel lblTituloCenas = new JLabel("Cenas");
+        lblTituloCenas.setBounds(140 + 2 * imgDado.getIconWidth(), 10,
+                100, 25);
+        lblTituloCenas.setHorizontalAlignment(SwingConstants.CENTER);
+        getContentPane().add(lblTituloCenas);
+
+        JLabel lblLanzamientos = new JLabel("0");
+        lblLanzamientos.setBounds(30 + 2 * imgDado.getIconWidth(), 40,
+                100, 100);
+        lblLanzamientos.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblLanzamientos.setFont(new Font("Tahoma", 1, 72));
+        lblLanzamientos.setBackground(new Color(0, 0, 0));
+        lblLanzamientos.setForeground(new Color(51, 255, 0));
+        lblLanzamientos.setOpaque(true);
+        getContentPane().add(lblLanzamientos);
+
+        JLabel lblCenas = new JLabel("0");
+        lblCenas.setBounds(140 + 2 * imgDado.getIconWidth(), 40,
+                100, 100);
+        lblCenas.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblCenas.setFont(new Font("Tahoma", 1, 72));
+        lblCenas.setBackground(new Color(0, 0, 0));
+        lblCenas.setForeground(new Color(51, 255, 0));
+        lblCenas.setOpaque(true);
+        getContentPane().add(lblCenas);
+
+        JButton btnIniciar = new JButton("Iniciar");
+        btnIniciar.setBounds(10, 20 + imgDado.getIconHeight(),
+                100, 25);
+        getContentPane().add(btnIniciar);
+
+        JButton btnLanzar = new JButton("Lanzar");
+        btnLanzar.setBounds(10, 60 + imgDado.getIconHeight(),
+                100, 25);
+        getContentPane().add(btnLanzar);
+
+        btnIniciar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                iniciarLanzamientos();
+            }
+
+        });
+
+        btnLanzar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lanzar();
+            }
+
+        });
+
+    }
+
+    private void iniciarLanzamientos() {
+        JOptionPane.showMessageDialog(null, "Hizo clic en Iniciar");
+    }
+
+    private void lanzar() {
+JOptionPane.showMessageDialog(null, "Hizo clic en Lanzar");
     }
 
 }
